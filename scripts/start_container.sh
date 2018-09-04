a1cc54734282ce4cc8255c236a5517bd402f1a49
Ghulfam Wani <ghulfam.wani@accoliteindia.com>
2018-09-04 17:01:26 +0530

    Removed redundent files

--- scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Container....."
-echo "Starting app.."
-python /root/app/docker-session/sample-app/tabledef.py
-python /root/app/docker-session/sample-app/dummy.py
-python /root/app/docker-session/sample-app/app.py
-#Start SSHD
-/usr/sbin/sshd -D
